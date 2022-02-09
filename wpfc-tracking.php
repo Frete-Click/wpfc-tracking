66df22b3fa80b61e75f9394e30e1321aa61d8933
devmunds <devmunds@gmail.com>
2022-02-09 15:50:14 -0300

    Update Description

--- wpfc-tracking.php
+++ wpfc-tracking.php
@@ -1,13 +1,16 @@
 <?php
 /**
- * Plugin Name:			WP Tracking - Via Frete Click
- * Description:			Rastreamento de mercadoreias via Frete Click. Shortcode: [wpfc-track]
+ * Plugin Name:			Frete Click - Tracking
+ * Description:			Consulte a situação da entrega de um ou mais objetos. Shortcode: [wpfc-track]
  * Version:				1.0.10
  * Author:				Frete Click
  * Author URI:			https://www.freteclick.com.br/
  * Requires at least:	5.0
  * Tested up to:		5.9
- *
+ * Requires PHP: 		7.0
+ * License: GPLv2 or later
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ * 
  * Text Domain: wpfc_tracking
  * Domain Path: /languages
  *
